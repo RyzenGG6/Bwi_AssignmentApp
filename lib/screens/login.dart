76a21b1ac7d4aa69135764dd43c8f65c1affade3
Sidharth <ryzen6644@gmail.com>
2023-11-15 22:05:16 +0530

    Update login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -33,11 +33,11 @@ class _loginState extends State<login> {
           )),
           Center(
             child: Container(
-              height: 300,
+              height: 389,
               width: screenWidth,
               color: Colors.white,
               padding: EdgeInsets.only(left: 20, right: 20, bottom: 40),
-              margin: EdgeInsets.only(top: screenHeight - 850),
+              margin: EdgeInsets.only(top: screenHeight - 880),
               child: Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(
@@ -46,66 +46,74 @@ class _loginState extends State<login> {
                 elevation: 5.0,
                 color: Colors.white,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Container(
-                      padding: EdgeInsets.only(left: 20, right: 20, top: 200),
-                      child: TextField(
-                        style: TextStyle(color: Colors.grey),
-                        decoration: InputDecoration(
-                            suffixIcon: Icon(
-                              Icons.call,
-                              color: Colors.grey,
-                            ),
-                            hintText: 'Enter Mobile Number',
-                            hintStyle: TextStyle(color: Colors.grey)),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 60),
-                      width: 280,
-                      height: 110,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Color(0xFF0E516C), // Button background color
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                10.0), // BorderRadius for rounded corners
+                      padding: EdgeInsets.only(left: 20, right: 20, top: 80),
+                      child:
+                      Column(
+                        children: [
+                          TextField(
+                            style: TextStyle(color: Colors.grey),
+                            decoration: InputDecoration(
+                                suffixIcon: Icon(
+                                  Icons.call,
+                                  color: Colors.grey,
+                                ),
+                                hintText: 'Enter Mobile Number',
+                                hintStyle: TextStyle(color: Colors.grey)),
                           ),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          'CONTINUE',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                          Container(
+                            padding: EdgeInsets.only(top: 60),
+                            width: 280,
+                            height: 110,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Color(0xFF0E516C), // Button background color
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      10.0), // BorderRadius for rounded corners
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                'CONTINUE',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(20),
+                            child: RichText(
+                              text: TextSpan(
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: 'By continuing you are agree to our \n',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                      text: 'Terms & Conditions ',
+                                      style: TextStyle(color: Colors.blueAccent)),
+                                  TextSpan(
+                                    text: 'and ',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                      text: ' Privacy Policy',
+                                      style: TextStyle(color: Colors.blueAccent)),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      )
                     ),
-                    Container(
-                      padding: EdgeInsets.all(20),
-                      child: RichText(
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'By continuing you are agree to our \n',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                            TextSpan(
-                                text: 'Terms & Conditions ',
-                                style: TextStyle(color: Colors.blueAccent)),
-                            TextSpan(
-                              text: 'and ',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                            TextSpan(
-                                text: ' Privacy Policy',
-                                style: TextStyle(color: Colors.blueAccent)),
-                          ],
-                        ),
-                      ),
-                    )
+
+
                     //                 Text('By continuing you are agree to our'),
                     // Text(Terms & Conditions )and Privacy Policy
                   ],
